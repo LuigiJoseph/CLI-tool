68b7af58c3bc9f94c0d7c1c2990b49aab024da7f
Mohammed Alyousuf <luigiornojoseph@gmail.com>
2024-08-09 23:19:18 +0100

    scan function complete

---
+++ scan.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"strings"
+)
+
+//scan given a apath craws it and its subfolders
+//searching for Git repos
+
+func scan(folder string) {
+	fmt.Printf("Found folders: \n\n")
+	repositories := recursiveScanFolder(folder)
+	filepath := getDotFilePath()
+	addNewSliceElementsToFile(filepath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+}
+
+//scanGitFolders returns a list of subfolders of 'folder' ending with '.git'
+//Returns the base folder of the repo, the .git folder parent
+//Recursively searches in the subfolders by passing as existsting 'folders' slice.
+
+func scanGitFolders(folders []string, folder string) []string {
+	//trim the last slash '/'
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	//optimise this later by checking if it's a git folder from the beginning
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "name_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+	return folders
+}
+
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+// returns dotfile for the repos list
+// creates and the enclosing folder if it does not exist
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+// given a slice of strings representing paths, stores them
+// to the filesystem
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringSliceToFile(repos, filePath)
+}
+
+// given a file path string, gets the content
+// of each line and parses to a slice of strings
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
+	return lines
+}
+
+func openFile(filePath string) *os.File {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		if os.IsNotExist((err)) {
+			//file does not exist
+			_, err = os.Create(filePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			//other error
+			panic(err)
+		}
+	}
+	return f
+}
+
+// adds the element of the 'new' slice
+// into the 'existing' only if not already there
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
+		}
+	}
+	return existing
+}
+
+func sliceContains(slice []string, value string) bool {
+	//the _ returns the index which we dont care about,
+	//we only care about the values
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func dumpStringSliceToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
+
+// stats generates a nice graph of your Git Contributions
+func stats(email string) {
+	print("stats")
+}
+
+func main() {
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
+}
