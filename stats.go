5fd5a4370df9f02d799974aba11553c1ae312c01
Mohammed Alyousuf <luigiornojoseph@gmail.com>
2024-08-16 19:40:41 +0100

    Process repos function

--- stats.go
+++ stats.go
@@ -5,8 +5,22 @@ func stats(email string) {
 	printCommitsStats(commits)
 }
 
+// given the user emai, return the commits he made in the past 6 months
 func processRepositories(email string) map[int]int {
+	filepath := getDotFilePath()
+	repos := parseFileLinesToSlice(filepath)
+	daysInMap := daysInLastSixMonths
 
+	commits := make(map[int]int, daysInMap)
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0
+	}
+
+	for _, path := range repos {
+		commits = fileCommits(email, path, commits)
+	}
+
+	return commits
 }
 
 func printCommitsStats(commits []string) {
