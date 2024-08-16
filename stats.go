0073a459b7ef1a1fae6fe6573ab5e695e83f678a
Mohammed Alyousuf <luigiornojoseph@gmail.com>
2024-08-16 19:36:40 +0100

    made basic templatesof functions

---
+++ stats.go
@@ -0,0 +1,14 @@
+package main
+
+func stats(email string) {
+	commits := processRepositories(email)
+	printCommitsStats(commits)
+}
+
+func processRepositories(email string) map[int]int {
+
+}
+
+func printCommitsStats(commits []string) {
+
+}
